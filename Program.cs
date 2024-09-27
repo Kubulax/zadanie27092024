6a7b3c52c9d0c39abd4d930cd5177a8e9ee281ef
jakub.koza <kubulax25@gmail.com>
2024-09-27 09:46:12 +0200

    Wykonano podpunkty 1 i 2, zaczęto podpunkt 3

--- Program.cs
+++ Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberOfDices = 0;
+
+            while (numberOfDices < 3 || numberOfDices > 10) 
+            {
+                Console.WriteLine("Ile kostek chcesz rzucić (3 - 10)");
+
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    numberOfDices = number;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            List<int> drawnDiceNumbers = new List<int>();
+
+            Random random = new Random();
+            for (int i = 0; i < numberOfDices; i++)
+            {
+                drawnDiceNumbers.Add(random.Next(1,7));
+            }
+
+            foreach (int number in drawnDiceNumbers)
+            {
+
+            }
         }
     }
 }
